6ab0a681dbd2e4f106d4d33c361354ce95e6f9e4
jaclynlipera <jac.lipera@gmail.com>
2018-07-25 15:45:05 -0400

    Css styling

--- dist/bundle.js
+++ dist/bundle.js
@@ -119,7 +119,6 @@ class Board {
     this.populate = this.populate.bind(this);
     this.findTrios = this.findTrios.bind(this);
     this.populate()
-    this.findTrios();
   }
 
 
@@ -131,67 +130,67 @@ class Board {
     }
     return this.grid
   }
-
-  findTrios() {
-    let trios = [];
-
-    //horizontal check
-    for (let i = 0; i < this.grid.length; i++) {
-      let vegCount = 1;
-      for (let j = 0; j < this.grid.length; j++) {
-          let checked = false;
-
-          if (i === this.grid.length - 1) {
-            checked = true;
-          } else {
-              if (this.grid[i][j].type === this.grid[i+1][j].type &&
-                this.grid[i][j].type !== -1) {
-                vegCount += 1;
-              } else {
-                checked = true;
-              }
-          }
-
-          if (checked) {
-              if (vegCount >= 3) {
-                  trios.push({ column: i+1-vegCount, row:j,
-                              length: vegCount, horizontal: true });
-              }
-              vegCount = 1;
-          }
-      }
-    }
-
-    //vertical check
-    for (let i = 0 ; i < this.grid.length; i++) {
-      let vegCount = 1;
-      for (let j = 0; j < this.grid.length; j++) {
-        let checked = false;
-
-        if (j === this.grid.length - 1) {
-          checked = true;
-        } else {
-          if (this.grid[i][j].type === this.grid[i][j + 1].type &&
-            this.grid[i][j].type !== -1) {
-            vegCount += 1;
-          } else {
-            checked = true;
-          }
-        }
-
-        if (checked) {
-          if (vegCount >= 3) {
-            trios.push({ column: i, row: j + 1 - vegCount,
-                          length: vegCount, horizontal: false });
-          }
-
-          vegCount = 1;
-        }
-      }
-    }
-
-    return trios;
-  }
+  // 
+  // findTrios() {
+  //   let trios = [];
+  //
+  //   //horizontal check
+  //   for (let i = 0; i < this.grid.length; i++) {
+  //     let vegCount = 1;
+  //     for (let j = 0; j < this.grid.length; j++) {
+  //         let checked = false;
+  //
+  //         if (i === this.grid.length - 1) {
+  //           checked = true;
+  //         } else {
+  //             if (this.grid[i][j].type === this.grid[i+1][j].type &&
+  //               this.grid[i][j].type !== -1) {
+  //               vegCount += 1;
+  //             } else {
+  //               checked = true;
+  //             }
+  //         }
+  //
+  //         if (checked) {
+  //             if (vegCount >= 3) {
+  //                 trios.push({ column: i+1-vegCount, row:j,
+  //                             length: vegCount, horizontal: true });
+  //             }
+  //             vegCount = 1;
+  //         }
+  //     }
+  //   }
+  //
+  //   //vertical check
+  //   for (let i = 0 ; i < this.grid.length; i++) {
+  //     let vegCount = 1;
+  //     for (let j = 0; j < this.grid.length; j++) {
+  //       let checked = false;
+  //
+  //       if (j === this.grid.length - 1) {
+  //         checked = true;
+  //       } else {
+  //         if (this.grid[i][j].type === this.grid[i][j + 1].type &&
+  //           this.grid[i][j].type !== -1) {
+  //           vegCount += 1;
+  //         } else {
+  //           checked = true;
+  //         }
+  //       }
+  //
+  //       if (checked) {
+  //         if (vegCount >= 3) {
+  //           trios.push({ column: i, row: j + 1 - vegCount,
+  //                         length: vegCount, horizontal: false });
+  //         }
+  //
+  //         vegCount = 1;
+  //       }
+  //     }
+  //   }
+  //
+  //   return trios;
+  // }
 
   // reorganizeBoard() {
   //   let numTrios = (this.grid.findTrios()).length;
